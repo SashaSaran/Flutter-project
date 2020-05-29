3df658d9aa906210897365ce6a231346d033bc70
sanliverpool13 <kairosh.sanjar@gmail.com>
2020-05-28 21:49:36 -0400

    started playing with a stateful widget

--- prototype_project/lib/main.dart
+++ prototype_project/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -9,16 +9,42 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    bool isButtonPressed = false;
     return MaterialApp(
       title: 'Welcome TO Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome To Flutter'),
           ),
-        body: Center(
-          child: Text('Hello World')
-        ),
+        body: ButtonWidgetState(),
       ),
     );
   }
 }
+
+class ButtonWidget extends StatefulWidget {
+  @override 
+  ButtonWidgetState createState() => ButtonWidgetState();
+}
+
+
+class ButtonWidgetState extends State<ButtonWidget> {
+  bool isButtonPressed = false;
+
+  @override 
+  Widget build(BuildContext context){
+    return RaisedButton(
+      color: isButtonPressed ? Colors.blue[100] : Colors.blue[50],
+      onPressed: (){
+        setState(() {
+          isButtonPressed = true;
+        });
+      },
+      child: Text('Sanjar Button'),
+    );
+  }
+}
+
+
+
+

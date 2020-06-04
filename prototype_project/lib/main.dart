ae9f1a954635b09aa7845cc0426bdfae677bda7e
Simon Ly <sim.only@hotmail.com>
2020-06-04 17:48:01 -0400

    added page change and map, but map has gradle problem

--- prototype_project/lib/main.dart
+++ prototype_project/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 
 void main() {
@@ -60,14 +62,44 @@ class SimonButtonState extends State<SimonButton> {
     return FloatingActionButton(
       backgroundColor: buttonColour,
       onPressed: (){
-        setState(() {
-          if (buttonColour == Colors.red) {
-            buttonColour = Colors.green;
-          } else {
-            buttonColour = Colors.red;
-          };
-        });
+        Navigator.push(context,
+        MaterialPageRoute(builder: (context) => MapScreen()));
       },
+    );
+  }
+}
+
+class MapScreen extends StatefulWidget {
+  @override
+  MapScreenState createState() => MapScreenState();
+}
+
+class MapScreenState extends State<MapScreen> {
+
+  static final CameraPosition initialLocation = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.476,
+  );
+  GoogleMapController mapCon;
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Map'),
+      ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: initialLocation,
+        onMapCreated: (GoogleMapController controller) {
+          mapCon = controller;
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: (){
+          Navigator.pop(context);
+        }
+      ),
     );
   }
 }
